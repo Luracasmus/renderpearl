b7c85d254297dc9d2cb2bcad105fcdfd04f8f292
Luracasmus <77991691+Luracasmus@users.noreply.github.com>
2025-11-25 18:08:07 +0100

    Update light level transformation for 1.21.10

--- shaders/lib/norm_uv2.glsl
+++ shaders/lib/norm_uv2.glsl
@@ -1,11 +1,11 @@
 in ivec2 vaUV2;
 
 // `vaUV2` scaled to [0, 1] from:
-// Terrain: [8, 240]
+// Terrain: [8, 248]
 // Entities, block entities, armor, player & hand: [0, 240]
 f16vec2 norm_uv2() {
 	#ifdef TERRAIN
-		return saturate(fma(f16vec2(vaUV2), f16vec2(1.0/232.0), f16vec2(-1.0/29.0)));
+		return saturate(fma(f16vec2(vaUV2), f16vec2(1.0/240.0), f16vec2(-1.0/30.0)));
 	#else
 		return f16vec2(vaUV2) * f16vec2(1.0/240.0);
 	#endif
