c230c4cb33038db05a85b8af18e0a239f252b0e6
Luracasmus <77991691+Luracasmus@users.noreply.github.com>
2026-01-14 18:56:46 +0100

    Enable Vanilla directional shading by default

--- shaders/prelude/config.glsl
+++ shaders/prelude/config.glsl
@@ -7,8 +7,8 @@
 	#define IND_BL 0.03 // [0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1]
 	#define AMBIENT 0.05 // [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5]
 	#define SSS 5 // [0 1 2 3 4 5 6 7 8 9 10]
+	#define VANILLA_SHADING
 	const float ambientOcclusionLevel = 1.0; // [0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0]
-	// #define VANILLA_SHADING
 
 	// SHADOW_MAP
 		const int shadowMapResolution = 2048; // [128 256 512 1024 2048 4096 8192]
