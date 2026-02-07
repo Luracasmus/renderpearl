54e9298c2f683d654ab7cbbf30349850592410f6
Luracasmus <77991691+Luracasmus@users.noreply.github.com>
2026-02-07 13:11:14 +0100

    Make shadow distortion configurable

--- shaders/prelude/config.glsl
+++ shaders/prelude/config.glsl
@@ -16,6 +16,7 @@
 		#define SM_DIST 10 // [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32]
 		#define SM_BLUR 2 // [0 1 2]
 		#define SM_FADE_DIST 0.15 // [0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5]
+		#define SM_DISTORTION 90 // [0 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95]
 		#define SM_ENTITY
 		#define SM_PLR
 		#define SM_BLOCK_ENTITY
